22e5d57540c03e52f74e998a172d778b62d78ec5
xiayang2333 <86812162+xiayang2333@users.noreply.github.com>
2022-01-02 13:13:42 +0800

    Add files via upload

---
+++ 第十次作业/Account.java
@@ -0,0 +1,36 @@
+package 第十次作业;
+
+import java.util.ArrayList;
+
+public class Account {
+    String name;
+    int id;
+    double last_money, interest;
+    ArrayList<Transactions> transactions = new ArrayList<>();
+
+    public Account(String name, int id, double last_money, double interest) {
+        this.name = name;
+        this.id = id;
+        this.last_money = last_money;
+        this.interest = interest;
+    }
+
+    public void withdraw(int money) {
+        this.last_money -= money;
+        Transactions temp = new Transactions('W', money, this.last_money, "nothing");
+        transactions.add(temp);
+    }
+
+    public void deposit(int money) {
+        this.last_money += money;
+        Transactions temp = new Transactions('D', money, this.last_money, "nothing");
+        transactions.add(temp);
+    }
+
+    public void tostring() {
+        System.out.println("Name: " + this.name + " interest: " + this.interest + " amount:" + this.last_money);
+        for (int i = 0; i < transactions.size(); i++) {
+            transactions.get(i).tostring();
+        }
+    }
+}
---
+++ 第十次作业/code11_17.java
@@ -0,0 +1,39 @@
+package 第十次作业;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class code11_17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("Enter a integer m: ");
+        int m = input.nextInt();
+        int yz = 2;
+        int temp = m;
+        while (temp != 1) {
+            // System.out.println(temp);
+            if (temp % yz == 0) {
+                list.add(yz);
+                temp /= yz;
+            } else {
+                yz++;
+            }
+        }
+        int ans = 1;
+        int i = 0;
+        yz = 2;
+        while (i < list.size()) {
+            if (list.contains(yz)) {
+                int num = list.lastIndexOf(yz) - i + 1;
+                i = list.lastIndexOf(yz) + 1;
+                if (num % 2 == 1) {
+                    ans *= yz;
+                }
+            }
+            yz++;
+        }
+        System.out.println("The smallest number n for m * n to be a perfect square is " + Math.sqrt(m * ans));
+        System.out.println("m * n is " + ans * m);
+    }
+}

7a649eb74ca5c4ff1a35ddc1e346b153cb078854
xiayang2333 <86812162+xiayang2333@users.noreply.github.com>
2022-01-02 13:13:16 +0800

    Add files via upload

---
+++ 第八次作业/PolymorphismDemo.java
@@ -0,0 +1,12 @@
+package 第八次作业;
+
+public class PolymorphismDemo {
+    public static void main(String[] args) {
+        displayObject(new Circle(1, "red", false));
+        displayObject(new Rectangle(1, 1, "black", true));
+    }
+
+    public static void displayObject(GeometricObject object) {
+        System.out.println("Create on " + object.getDateCreated() + ". Color is " + object.getcolor());
+    }
+}
